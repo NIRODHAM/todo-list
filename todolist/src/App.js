5cefa9cc0f9f1ff4d706476eea63b3d2b99c6e35
nirodha <nirodhamadusanka1234@gmail.com>
2026-01-05 07:57:19 +0530

    make the structure

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [display, setDisplay] = useState("");
+
+  function addText(event) {
+    setInputText(event.target.value);
+  }
+
+  function displayText() {
+    setDisplay(inputText);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" onChange={addText} />
+      <button onClick={displayText}>click</button>
+
+      <p>{display}</p>
     </div>
   );
 }
